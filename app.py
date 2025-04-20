43e8a710403c65dbddbad3c708dc0f3363405653
AnitaMaxWynn901 <165561635+AnitaMaxWynn901@users.noreply.github.com>
2025-04-20 16:43:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,11 @@ from sklearn.datasets import make_blobs
 with open('kmeans_model.pkl', 'rb') as f:
     loaded_model = pickle.load(f)
     
-#set the title
-st.title("K-Means Clustering Visualizer by haenainglay")
-
 #set the page
 st.set_page_config(page_title = "K-Means Clustering", layout = "centered")
+
+#set the title
+st.title("K-Means Clustering Visualizer by haenainglay")
 
 #load_dataset
 X, _ = make_blobs(n_samples=300, centers=loaded_model.n_clusters, cluster_std=0.60, random_state=0)
